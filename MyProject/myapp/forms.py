542dbdf34573802ad7b156e269db78f71ce86957
Pardallzim <rogerio.silva220304@gmail.com>
2024-08-01 22:16:22 -0300

    Multi_Users

---
+++ MyProject/myapp/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from myapp.models import *
+from django.contrib.auth import get_user_model
+
+CustomUser = get_user_model()
+
+
+class ItensForm(forms.ModelForm):
+    class Meta:
+
+        model = Itens
+        fields = "__all__"
+        labels = {
+            "name": "nome",
+            "descript": "descrição",
+            "path": "imagem",
+        }
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': "Nome do item",
+                }
+            ),
+            'descript': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': "Escreva uma breve descrição",
+                }
+            ),
+            'path': forms.ClearableFileInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': "Imagem",
+                }
+            ),
+        }
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username','password1','password2','user_type')
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
